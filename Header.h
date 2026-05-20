fc70d460abf04625c67a31876988dda958620dd0
Pova99 <124100681+Pova99@users.noreply.github.com>
2026-05-20 09:39:05 +0200

    Update Header.h

--- Header.h
+++ Header.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#define CAT_LEN 32
+#define NAME_LEN 64
+#define MANU_LEN 64
+#define DAT_BIN "parts.bin"
+#define TEMP_BIN "temp.bin"
+
 static char* filename = "parts.bin";
 
 typedef enum {
@@ -12,19 +18,22 @@ typedef enum {
 } PART_CATEGORY;
 
 typedef struct {
-    char catalog_number[32];
-    char name[64];
-    char manufacturer[64];
+    char catalog_number[CAT_LEN];
+    char name[NAME_LEN];
+    char manufacturer[MANU_LEN];
     double price;
     int quantity;
     PART_CATEGORY category;
 } PART;
 
 int menu(FILE*);
-void create_parts(FILE*, PART*);
-void read_parts(FILE*);
-void update_parts(FILE*, const char*);
-void delete_parts(FILE*, const char*);
-void find_part(FILE*, const char*, PART*);
+void create_parts(FILE*, PART*, int*);
+void read_parts(FILE*, PART*);
+void update_parts(FILE*, const char*, PART*);
+void delete_parts(FILE*, const char*, PART*);
+PART* find_part(FILE*, const char*, PART*);
 
 PART* enter_parts_info();
+void print_engine_parts(FILE*, PART*);
+void format_print_parts(PART*);
+FILE* get_all_data(FILE* fp, int*);
