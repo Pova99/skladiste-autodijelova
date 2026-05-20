60df941bc5d6e7491e320fe54cfc3f69ea3d9209
Pova99 <124100681+Pova99@users.noreply.github.com>
2026-05-20 09:39:37 +0200

    Update functions_misc.c

--- functions_misc.c
+++ functions_misc.c
@@ -35,24 +35,84 @@ PART* enter_parts_info() {
 	return p;
 }
 
-void print_engine_parts(FILE* fp) {
-
-	PART p = { 0 };
+void print_engine_parts(FILE* fp, PART* p) {
 
 	rewind(fp);
 
 	printf("\n\t\t=== ENGINE PARTS ===\n");
 
-	while (fread(&p, sizeof(PART), 1, fp) == 1) {
+	while (fread(p, sizeof(PART), 1, fp) == 1) {
 
-		if (p.category == ENGINE) {
+		if (p->category == ENGINE) {
 
-			printf("Catalog: %s\n", p.catalog_number);
-			printf("Name: %s\n", p.name);
-			printf("Manufacturer: %s\n", p.manufacturer);
-			printf("Price: %.2f\n", p.price);
-			printf("Quantity: %d\n", p.quantity);
+			printf("Catalog: %s\n", p->catalog_number);
+			printf("Name: %s\n", p->name);
+			printf("Manufacturer: %s\n", p->manufacturer);
+			printf("Price: %.2f\n", p->price);
+			printf("Quantity: %d\n", p->quantity);
 			printf("----------------------\n");
+		}
+	}
+}
+
+void format_print_parts(PART* p) {
+
+	printf("%-20s | ", p->catalog_number);
+
+	switch (p->category) {
+	case ENGINE:
+		printf("ENGINE");
+		break;
+
+	case BRAKES:
+		printf("BRAKES");
+		break;
+
+	case SUSPENSION:
+		printf("SUSPENSION");
+		break;
+
+	case ELECTRICAL:
+		printf("ELECTRICAL");
+		break;
+
+	case BODY:
+		printf("BODY");
+		break;
+
+	default:
+		printf("OTHER");
+		break;
+	}
+
+	printf(" | %-20s | %-15s | %-8.2f | %-5d\n", p->name,
+		p->manufacturer, p->price, p->quantity);
+}
+
+FILE* get_all_data(FILE* fp, int* num) {
+
+	if (fp == NULL) {
+
+		perror("Datoteka ne postoji!");
+		return;
+	}
+	else {
+
+		rewind(fp);
+		fread(num, sizeof(int), 1, fp);
+
+		PART* p = (PART*)calloc(*num, sizeof(PART));
+
+		if (p == NULL) {
+
+			free(p);
+			perror("Zauzimanje memorije!");
+			exit(EXIT_FAILURE);
+		}
+		else {
+
+			fread(p, sizeof(PART), *num, fp);
+			return p;
 		}
 	}
 }
