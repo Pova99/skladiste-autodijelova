38aa6399f65d7b5c10c8ca7a86ea4a8779384ebe
Pova99 <124100681+Pova99@users.noreply.github.com>
2026-05-20 09:39:21 +0200

    Update cruid.c

--- cruid.c
+++ cruid.c
@@ -5,7 +5,7 @@
 #include <errno.h>
 #include "Header.h"
 
-void create_parts(FILE* fp, PART* p) {
+void create_parts(FILE* fp, PART* p, int* num) {
 
 	if (fp == NULL) {
 
@@ -13,11 +13,15 @@ void create_parts(FILE* fp, PART* p) {
 		return;
 	}
 
+	fread(&num, sizeof(int), 1, fp);
+	rewind(fp);
+	num++;
+	fwrite(&num, sizeof(int), 1, fp);
 	fseek(fp, 0, SEEK_END);
 	fwrite(p, sizeof(PART), 1, fp);
 }
 
-void read_parts(FILE* fp) {
+void read_parts(FILE* fp, PART* p) {
 	
 	if (fp == NULL) {
 
@@ -27,54 +31,22 @@ void read_parts(FILE* fp) {
 
 	rewind(fp);
 
-	PART p = { 0 };
-	while (fread(&p, sizeof(PART), 1, fp)) {
+	while (fread(p, sizeof(PART), 1, fp)) {
 
-		printf("%-20s | ", p.catalog_number);
-
-		switch (p.category) {
-		case ENGINE:
-			printf("ENGINE");
-			break;
-
-		case BRAKES:
-			printf("BRAKES");
-			break;
-
-		case SUSPENSION:
-			printf("SUSPENSION");
-			break;
-
-		case ELECTRICAL:
-			printf("ELECTRICAL");
-			break;
-
-		case BODY:
-			printf("BODY");
-			break;
-
-		default:
-			printf("OTHER");
-			break;
-		}
-
-		printf(" | %-20s | %-15s | %-8.2f | %-5d\n", p.name,
-			p.manufacturer, p.price, p.quantity);
+		format_print_parts(p);
 	}
 	printf("\n");
 }
 
-void find_part(FILE* fp, const char* id, PART* result) {
+PART* find_part(FILE* fp, const char* id, PART* p) {
 
 	rewind(fp);
 
-	PART p = { 0 };
+	while (fread(p, sizeof(PART), 1, fp)) {
 
-	while (fread(&p, sizeof(PART), 1, fp)) {
-
-		if (strcmp(p.catalog_number, id) == 0) {
+		if (strcmp(p->catalog_number, id) == 0) {
 			
-			*result = p;
+			format_print_parts(p);
 			return 1;
 		}
 	}
@@ -82,7 +54,7 @@ void find_part(FILE* fp, const char* id, PART* result) {
 	return 0;
 }
 
-void update_parts(FILE* fp, const char* catalog_number) {
+void update_parts(FILE* fp, const char* catalog_number, PART* p) {
 	
 	if (fp == NULL) {
 
@@ -92,18 +64,17 @@ void update_parts(FILE* fp, const char* catalog_number) {
 
 	rewind(fp);
 
-	PART p = { 0 };
-	while (fread(&p, sizeof(PART), 1, fp)) {
+	while (fread(p, sizeof(PART), 1, fp)) {
 
-		if (strcmp(p.catalog_number, catalog_number) == 0) {
+		if (strcmp(p->catalog_number, catalog_number) == 0) {
 
 			fseek(fp, -1, SEEK_CUR);
 
 			printf("Unesi novu cijenu: \n");
-			if (scanf("%lf %d", &p.price) != 1) while (getchar() != '\n');
+			if (scanf("%lf", &p->price) != 1) while (getchar() != '\n');
 
 			printf("Unesi novu kolicinu: \n");
-			if (scanf("%lf %d", &p.quantity) != 1) while (getchar() != '\n');
+			if (scanf("%d", &p->quantity) != 1) while (getchar() != '\n');
 
 			fwrite(&p, sizeof(PART), 1, fp);
 			return;
@@ -111,24 +82,23 @@ void update_parts(FILE* fp, const char* catalog_number) {
 	}
 }
 
-void delete_parts(FILE* fp, const char* catalog_number) {
+void delete_parts(FILE* fp, const char* catalog_number, int* num, PART* p) {
 
-	FILE* temp = fopen("temp.dat", "wb");
+	FILE* temp = fopen(TEMP_BIN, "wb");
+
 	rewind(fp);
 
-	PART p = { 0 };
+	while (fread(p, sizeof(PART), 1, fp)) {
 
-	while (fread(&p, sizeof(PART), 1, fp)) {
+		if (strcmp(p->catalog_number, catalog_number) != 0) {
 
-		if (strcmp(p.catalog_number, catalog_number) != 0) {
-
-			fwrite(&p, sizeof(PART), 1, temp);
+			fwrite(p, sizeof(PART), 1, temp);
 		}
 	}
 
 	fclose(fp);
 	fclose(temp);
 
-	remove("parts.dat");
-	rename("temp.dat", "parts.dat");
+	remove(DAT_BIN);
+	rename(TEMP_BIN, DAT_BIN);
 }
